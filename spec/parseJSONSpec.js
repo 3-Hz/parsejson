47bf3265ce4054d4078ed4a22258276db56904cb
3-Hz <edward.h.zhao@gmail.com>
2020-11-26 02:37:36 -0600

    Fully functional

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -4,6 +4,7 @@ describe('parseJSON', function() {
   it('should match the result of calling JSON.parse', function() {
     parseableStrings.forEach(function(test) {
       var result = parseJSON(test);
+      //console.log(result);
       var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
@@ -20,7 +21,7 @@ describe('parseJSON', function() {
       // if you'd prefer, you can write your version of parseJSON
       // so that it passes this test instead of the one on line 21.
       // expect(parseJSON(test)).to.equal(undefined);
-      expect(fn).to.throw(SyntaxError);
+      expect(fn).to.throw('SyntaxError');
     });
   });
 
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,3 +1,188 @@
 var parseJSON = function(json) {
+  let i = 0;
+  return parseValue();
 
+  function parseValue() {
+    skipWhiteSpace();
+    const value =
+      parseString() ??
+      parseNumber() ??
+      parseObject() ??
+      parseArray() ??
+      parseKeyword('true', true) ??
+      parseKeyword('false', false) ??
+      parseKeyword('null', null);
+    skipWhiteSpace();
+    return value;
+  };
+
+  function parseObject() {
+    if (json[i] === '{') {
+      i++;
+      skipWhiteSpace();
+
+      const result = {};
+
+      let initial = true;
+
+      while (json[i] !== '}') {
+        checkForUnexpectedEndOfInput()
+        if (!initial) {
+          skipComma();
+          skipWhiteSpace();
+        }
+        const key = parseString();
+        skipWhiteSpace();
+        skipColon();
+        const value = parseValue();
+        result[key] = value;
+        initial = false;
+      }
+      i++;
+      return result;
+    }
+  };
+
+  function parseArray() {
+    if (json[i] === '[') {
+      i++;
+      skipWhiteSpace();
+
+      const result = [];
+
+      let initial = true;
+
+      while (json[i] !== ']') {
+        checkForUnexpectedEndOfInput()
+        if (!initial) {
+          skipComma();
+        }
+        const value = parseValue();
+        result.push(value);
+        initial = false;
+      }
+      i++;
+      return result;
+    }
+  };
+
+  function parseString() {
+    if (json[i] === '"') {
+      i++;
+      let result = '';
+      while (json[i] !== '"') {
+        checkForUnexpectedEndOfInput()
+        if (json[i] === '\\') {
+          const char = json[i + 1];
+          if (
+            char === '"' ||
+            char === '\\' ||
+            char === '/' ||
+            char === 'b' ||
+            char === 'f' ||
+            char === 'n' ||
+            char === 'r' ||
+            char === 't'
+          ) {
+            result += char;
+            i++;
+          } else if (char === 'u') {
+            if (
+              isHexadecimal(json[i + 2]) &&
+              isHexadecimal(json[i + 3]) &&
+              isHexadecimal(json[i + 4]) &&
+              isHexadecimal(json[i + 5])
+            ) {
+              result += String.fromCharCode(
+                parseInt(json.slice(i + 2, i + 6), 16)
+              );
+              i += 5;
+            }
+          }
+        } else {
+          result +=  json[i];
+        }
+        i++;
+      }
+      i++;
+      return result;
+    }
+  };
+
+  function parseNumber() {
+    let start = i;
+    if (json[i] === '-') {
+      i++;
+    }
+    if (json[i] === '0') {
+      i++;
+    } else if (json[i] >= '1' && json[i] <= '9') {
+      i++;
+      while (json[i] >= '0' && json[i] <= '9') {
+        checkForUnexpectedEndOfInput()
+        i++;
+      }
+    }
+
+    if (json[i] === '.') {
+      i++;
+      while (json[i] >= '0' && json[i] <= '9') {
+        checkForUnexpectedEndOfInput()
+        i++;
+      }
+    }
+    if (json[i] === 'e' || json[i] === 'E') {
+      i++;
+      if (json[i] === '-' || json[i] === '+') {
+        i++;
+      }
+      while (json[i] >= '0' && json[i] <= '9') {
+        checkForUnexpectedEndOfInput()
+        i++;
+      }
+    }
+    if (i > start) {
+      return Number(json.slice(start, i));
+    }
+  };
+
+  function parseKeyword(name, value) {
+    if (json.slice(i, i + name.length) === name) {
+      i += name.length;
+      return value;
+    }
+  };
+
+  function checkForUnexpectedEndOfInput() {
+    if (json[i] === undefined) {
+      throw new Error('SyntaxError');
+    }
+  };
+
+  function isHexadecimal(char) {
+    return (
+      (char >= '0' && char <= '9') ||
+      (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'f')
+    );
+  };
+
+  function skipComma() {
+    if (json[i] !== ',') {
+      throw new Error('SyntaxError');
+    }
+    i++;
+  };
+
+  function skipColon() {
+    if (json[i] !== ':') {
+      throw new Error('SyntaxError');
+    }
+    i++;
+  };
+
+  function skipWhiteSpace() {
+    while (json[i] === ' ' || json[i] === '\r' || json[i] === '\n' || json[i] === '\t') {
+      i++;
+    }
+  };
 };
